19acfbb26518fc52ed4470901605dca59d97efed
Ismael Padilla <padillaismael92@gmail.com>
2021-03-14 22:17:32 -0300

    Comments, -dm option

--- main.py
+++ main.py
@@ -12,18 +12,27 @@ bot = commands.Bot(command_prefix=prefix, intents=intents)
 
 
 @bot.command()
-async def shuffle(ctx, arg):
+async def shuffle(ctx, arg, *args):
+    '''
+    Shuffle and print list of items.
+    Usage: 'shuffle audio' to shuffle the members of the audio channel you're currently inf.
+
+    Options: -dm to send the result to your dms.
+    '''
     user = ctx.author
+    sendTo = ctx
+    if '-dm' in args:
+        sendTo = user
     if arg == 'audio':
         # get audio channel the user is in
         if (user.voice != None):
-            await ctx.send('shuffling members in ' + user.voice.channel.name + '...')
+            await sendTo.send('Shuffling members in ' + user.voice.channel.name + '...')
             # get list of members, shuffle it and send it
             channelMembers = user.voice.channel.members
             random.shuffle(channelMembers)
             for member in channelMembers:
-                await ctx.send(member.name)
+                await sendTo.send(member.name)
         else:
-            await ctx.send(user.name + " isn't in any audio channel!")
+            await sendTo.send(user.name + " isn't in any audio channel!")
 
 bot.run(os.getenv('TOKEN'))
