6df24fc77c9387e5705cac8317ec1c404961ec6c
ismaelpadilla <padillaismael92@gmail.com>
2022-04-07 13:15:30 -0300

    Add support for roles

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ class Source(commands.Converter):
     Converter to convert input of other methods to something that we can shuffle or pick from
     '''
     async def convert(self, ctx, argument):
-        if argument in ('audio', 'list'):
+        if argument in ('audio', 'list', 'role'):
             return argument
-        raise BadArgument()
+        raise commands.BadArgument()
 
 
 @bot.command(description="Shuffle and print list of items.", usage="item1 item2 item3", brief="Shuffle a list of items (i.e. '$shuffle item1 item2 item3')")
@@ -28,6 +28,7 @@ async def shuffle(ctx, arg: typing.Optional[Source] = 'list', *args):
     Usage: 
     'shuffle list item1 item2 item3' to shuffle items in list. 'shuffle item1 item2 item3' also works.
     'shuffle audio' to shuffle the members of the audio channel you're currently in.
+    'shuffle role @role1 role2' to shuffle the members in roles @role1 and @role2.
 
     Options: -dm to send the result to your dms.
     '''
@@ -60,6 +61,31 @@ async def shuffle(ctx, arg: typing.Optional[Source] = 'list', *args):
         else:
             await sendTo.send("You didn't specify a list to shuffle.\n" +
                               "Correct usage is `{0}shuffle list item1 item2 item3`.\nYou can also use `{0}shuffle audio` to shuffle the members of the audio channel you're currently in.".format(prefix))
+    if arg == 'role':
+        # these can't be used in dms
+        if ctx.guild == None:
+            await sendTo.send("This only works in a server's channel")
+            return
+
+        # must specify at least one role
+        if len(args) == 0:
+            await sendTo.send("You didn't specify any roles to shuffle.\n" +
+                              "Correct usage is `{0}shuffle role @role1 @role2 @role3`.".format(prefix))
+            return
+
+        message = await sendTo.send('Shuffling members in roles...')
+
+        roles = []
+
+        for arg in args:
+            converted = await commands.RoleConverter().convert(ctx, arg)
+            roles.append(converted)
+
+        users = set()
+        for rol in roles:
+            users.update([member.display_name for member in rol.members])
+
+        await shuffleAndSend(list(users), message)
 
 
 async def shuffleAndSend(items, message):
@@ -74,6 +100,7 @@ async def pick(ctx, arg: typing.Optional[Source] = 'list', quantity: typing.Opti
     Usage:
     'pick list 2 item1 item2 item3' to pick 2 items from the list (item1, item2, item3). 'pick 2 item1 item2 item3' also works.
     'pick audio 3' to pick 3 users from the audio channel you're currently in.
+    'pick role 4 @role1 @role2' to pick 4 users from roles @role1 and @role2.
     If you don't specify a number, one item will be chosen (i.e. 'pick item1 item2 item3')
 
     Options: -dm to send the result to your dms.
@@ -109,6 +136,31 @@ async def pick(ctx, arg: typing.Optional[Source] = 'list', quantity: typing.Opti
                 await sendTo.send("Number of items to pick must be at least 1.\nCorrect usage is `{0}pick 1 item1 item2 item3`.".format(prefix))
         else:
             await sendTo.send("You didn't specify a list to pick from.\nCorrect usage is `{0}pick item1 item2 item3`.".format(prefix))
+    if arg == 'role':
+        # these can't be used in dms
+        if ctx.guild == None:
+            await sendTo.send("This only works in a server's channel")
+            return
+
+        # must specify at least one role
+        if len(args) == 0:
+            await sendTo.send("You didn't specify any roles to pick from.\n" +
+                              "Correct usage is `{0}pick role @role1 @role2 @role3`.".format(prefix))
+            return
+
+        message = await sendTo.send('Picking from members in roles...')
+
+        roles = []
+
+        for arg in args:
+            converted = await commands.RoleConverter().convert(ctx, arg)
+            roles.append(converted)
+
+        users = set()
+        for rol in roles:
+            users.update([member.display_name for member in rol.members])
+
+        await pickAndSend(list(users), quantity, message)
 
 
 async def pickAndSend(items, quantity, message):
